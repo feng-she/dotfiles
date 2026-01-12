8c261607b468626b7c86e3001b9531b0074444f1
chenpeijian <chen_peijian@163.com>
2026-01-12 14:55:36 +0800

    增加代码跳转的快捷键

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,24 @@
 -- Add any additional keymaps here
 
 vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
+
+local function def_vsplit()
+  vim.cmd("vsplit")
+  vim.cmd("wincmd l") -- 确保到右窗
+  vim.lsp.buf.definition({ reuse_win = false })
+end
+
+local function def_split()
+  vim.cmd("split")
+  vim.cmd("wincmd j") -- 确保到下窗
+  vim.lsp.buf.definition({ reuse_win = false })
+end
+
+local function def_tab()
+  vim.cmd("tab split")
+  vim.lsp.buf.definition({ reuse_win = false })
+end
+
+vim.keymap.set("n", "gV", def_vsplit, { desc = "Goto Definition (vsplit)" })
+vim.keymap.set("n", "gH", def_split, { desc = "Goto Definition (split)" })
+vim.keymap.set("n", "gT", def_tab, { desc = "Goto Definition (tab)" })
